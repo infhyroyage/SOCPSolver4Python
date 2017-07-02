24f62dfe1b9415feb6f454972fce4cebfa098f7f
Kido Takeru <keidy2463@gmail.com>
2017-07-03 01:01:13 +0900

    Add Infeasible and Unbounded SOCP

--- socp_cvxopt.py
+++ socp_cvxopt.py
@@ -19,5 +19,38 @@ def solve_socp1():
 	print(sol['x'])
 
 
+def solve_socp_infeasible():
+	e = np.array([1.0, 1.0])
+	c = co.matrix(e)
+
+	g1 = np.array([[0.0, 0.0], [-1.0, 0.0], [0.0, -0.5]])
+	g2 = np.array([[-0.5, 0.0], [0.0, -1.0 / sqrt(3)], [0.0, 0.0]])
+	g = [co.matrix(g1), co.matrix(g2)]
+
+	h1 = np.array([1.0, 0.0, 0.0])
+	h2 = np.array([0.0, 0.0, 1.0])
+	h = [co.matrix(h1), co.matrix(h2)]
+
+	sol = co.solvers.socp(c, Gq=g, hq=h)
+	print(sol['x'])
+
+
+def solve_socp_unbounded():
+	e = np.array([-1.0, -1.0, -1.0])
+	c = co.matrix(e)
+
+	g1 = np.array([[0.0, 0.0, -1.0], [-1.0, 0.0, 0.0], [0.0, -1.0, 0.0]])
+	g = [co.matrix(g1)]
+
+	h1 = np.array([0.0, 0.0, 0.0])
+	h = [co.matrix(h1)]
+
+	sol = co.solvers.socp(c, Gq=g, hq=h)
+	print(sol['x'])
+
+
 if __name__ == '__main__':
-	solve_socp1()
+	print("[Infeasible SOCP]")
+	solve_socp_infeasible()
+	print("[Unbounded SOCP]")
+	solve_socp_unbounded()
